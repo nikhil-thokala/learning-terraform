e8fd76ab3e72b1a2f0b33e8ced30cdf3dc53df4f
nikhilt <nikhil.t1226@gmail.com>
2023-03-19 13:13:04 +0000

    \ |"?

--- 4.vars/sample.tf
+++ 4.vars/sample.tf
@@ -1,20 +1,29 @@
 variable "sample" {
-  default = "Hello World"
+  default = "Hello World"  # strings must be double quote
 }
 
 output "sample" {
   value = var.sample
 }
 
+
+variable "sample01" {
+  default = 100           # no need to quote with single or double for numbers and boolean
+}
+
+output "sample01" {
+  value = var.sample
+}
+
 variable "sample1" {}
 
-output "sample1" {
+output "sample1" {       # input is given in terraform.vars file
   value = var.sample1
 }
 
 variable "cli" {}
 
-output "cli" {
+output "cli" {           # input have to provide in cli
   value = var.cli
 }
 
