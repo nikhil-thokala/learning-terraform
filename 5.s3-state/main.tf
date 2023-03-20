f70ee941059ceaa7191fe3c581c1cad6a7962d95
nikhilt <nikhil.t1226@gmail.com>
2023-03-20 15:47:07 +0000

    \ |"?

---
+++ 5.s3-state/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-dvp"
+    key    = "5.s3-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws instance" "ec2" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "+3.micro"
+  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  tags                   = {
+    Name = "test"
+  }
+}
