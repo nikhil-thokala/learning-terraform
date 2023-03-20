3f0416515ffb3e62f3c1127967fb747a8279179a
nikhilt <nikhil.t1226@gmail.com>
2023-03-20 22:08:38 +0000

    \ |"?

--- roboshop.setup/ex4/ec2/main.tf
+++ roboshop.setup/ex4/ec2/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "ec2" {
 
 resource "aws_route53_record" "record" {
   zone_id = "Z0834449398HDP390BFHD"
-  name    = "$(var.component)-dev.devopsjob.online"
+  name    = "${var.component}-dev.devopsjob.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.ec2.private_ip]
