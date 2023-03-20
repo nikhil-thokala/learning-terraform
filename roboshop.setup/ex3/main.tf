6b82b70edb0836e96c5116b5fc66babecaa5c70b
nikhilt <nikhil.t1226@gmail.com>
2023-03-20 19:13:44 +0000

    \ |"?

--- roboshop.setup/ex3/main.tf
+++ roboshop.setup/ex3/main.tf
@@ -6,8 +6,8 @@ data "aws_ami" "ami" {
 
 resource "aws_instance" "frontend" {
   for_each               = var.instances
-  ami                    = "ami-0089b8e98cd95257d"
-  instance_type          = each.value.["type"]
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = each.value["type"]
   vpc_security_group_ids = ["sg-0443b175ebd7809fa"]
   tags = {
     Name = each.value["name"]
@@ -18,11 +18,11 @@ variable "instances" {
   default = {
     catalogue = {
     name ="catalogue"
-    type = t3.micro
+    type = "t3.micro"
     }
     user = {
-      name = user
-      type = t3.small
+      name = "user"
+      type = "t3.small"
     }
   }
 }
