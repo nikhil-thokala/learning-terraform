b10959c83c49b8b452eeccdaec7aacff37e3c5a3
nikhilt <nikhil.t1226@gmail.com>
2023-03-20 21:32:35 +0000

    \ |"?

--- 7.conditions/main.tf
+++ 7.conditions/main.tf
@@ -7,7 +7,7 @@ variable "instance_type" {}
 
 variable "create_instance" {}
 
-resource "aws_instance" "test" {
+resource "aws_instance" "test1" {
   count =var.create_instance == "true" ? 1 : 0
   ami = "ami-0089b8e98cd95257d"
   instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
