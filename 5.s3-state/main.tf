b5936780e3e7746c283cd15132ef7db29d0e9c48
nikhilt <nikhil.t1226@gmail.com>
2023-03-20 15:50:40 +0000

    \ |"?

--- 5.s3-state/main.tf
+++ 5.s3-state/main.tf
@@ -6,7 +6,7 @@ terraform {
   }
 }
 
-resource "aws instance" "ec2" {
+resource "aws_instance" "ec2" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "+3.micro"
   vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
