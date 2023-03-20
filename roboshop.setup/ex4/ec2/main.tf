9911723615bc336dbdcf06a10243ffd848fc1c14
nikhilt <nikhil.t1226@gmail.com>
2023-03-20 21:58:11 +0000

    \ |"?

---
+++ roboshop.setup/ex4/ec2/main.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "ec2" {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = ["sg-0443b175ebd7809fa"]
+  tags   = {
+    Name = var.component
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z0834449398HDP390BFHD"
+  name    = "$(var.component)-dev.devopsjob.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
+variable "instance_type" {}
