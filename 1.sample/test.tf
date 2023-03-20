7c018ff54f47072899db021874d4164badd57f07
nikhilt <nikhil.t1226@gmail.com>
2023-03-20 09:05:53 +0000

    \ |"?

---
+++ 1.sample/test.tf
@@ -0,0 +1,8 @@
+resource "aws instance" "ec2" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "+3.micro"
+  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  tags                   = {
+    Name = "test"
+  }
+}
