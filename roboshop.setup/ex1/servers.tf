28779734f3c1effcb525042e336888209c678ee9
nikhilt <nikhil.t1226@gmail.com>
2023-03-20 16:46:41 +0000

    \ |"?

--- roboshop.setup/ex1/servers.tf
+++ roboshop.setup/ex1/servers.tf
@@ -1,3 +1,9 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
 resource "aws_instance" "frontend" {
   ami                    = "ami-0089b8e98cd95257d"
   instance_type          = "t3.micro"
---
+++ roboshop.setup/ex2/main.tf
@@ -0,0 +1,12 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "frontend" {
+  count                  = 5
+  ami                    = "ami-0089b8e98cd95257d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0443b175ebd7809fa"]
+}
