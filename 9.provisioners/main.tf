aa1a08c5e798eb4235016fc61f078050a5d15899
nikhilt <nikhil.t1226@gmail.com>
2023-03-22 13:19:09 +0000

    \ |"?

---
+++ 9.provisioners/main.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0089b8e98cd95257d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0443b175ebd7809fa"]
+  tags                   = {
+    Name = "demo"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host        = self.public_ip
+      user        = "root"
+      password    = "Devops321"
+    }
+
+    inline = [
+    "echo Hello"
+    ]
+  }
+}
