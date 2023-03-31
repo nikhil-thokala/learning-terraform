e3c63b38edf04b01fc17ff4e5f407a787bb77f4e
nikhilt <nikhil.t1226@gmail.com>
2023-03-31 12:58:10 +0100

    \ |"?

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -9,6 +9,13 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
+
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
+}
+
 resource "aws_spot_instance_request" "node2" {
   ami           = "ami-017890b2db8994285"
   instance_type = "t3.micro"
@@ -17,4 +24,11 @@ resource "aws_spot_instance_request" "node2" {
   tags = {
     Name = "prom-test-node"
   }
+}
+
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
 }
